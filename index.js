71a840a7322c3d91cac89636d6deefb2e9f81444
Awesamm Coder <awesammcoder@gmail.com>
2018-08-23 06:35:21 +0800

    Used ExpressJS server and added CORS

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const PeerServer = require('peer').PeerServer;
-const server = PeerServer({port: process.env.OPENSHIFT_NODEJS_PORT || 5000, path: '/server'});
-console.log("Running to port:" + process.env.OPENSHIFT_NODEJS_PORT || 5000);
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 4000;
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) { res.json({
+  port: port
+}); });
+
+var server = app.listen(port);
+var options = {
+    debug: true
+}
+
+var peerserver = ExpressPeerServer(server, options);
+app.use('/server', peerserver);
